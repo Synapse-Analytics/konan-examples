92ada4fa56d99c59f5fc015f54afc7a56702f547
Nourhan Khaled <nourhan.kh02@gmail.com>
2021-01-14 15:46:28 +0100

    add response validators

--- python/endpoints.py
+++ python/endpoints.py
@@ -3,12 +3,13 @@ References:
 fastapi docs: https://fastapi.tiangolo.com/
 more on types: https://fastapi.tiangolo.com/python-types/
 fastapi vs flask: https://testdriven.io/blog/moving-from-flask-to-fastapi/#:~:text=Its%20popularity%20is%20fueled%20by,amongst%20the%20machine%20learning%20community.&text=Unlike%20Flask%2C%20FastAPI%20is%20an,fastest%20Python%2Dbased%20web%20frameworks.
+pydantic validation: https://pydantic-docs.helpmanual.io/usage/
 """
 
 import uvicorn
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, ValidationError
 
 # import the types you need
 from typing import Optional, List, Set, Tuple, Dict
@@ -28,6 +29,15 @@ class Request(BaseModel):
     other_feat: int
     optional_field: Optional[bool] = None # default value
 
+    # TODO: add validators to enforce value ranges
+    @validator('some_feat')
+    def in_values(cls, v):
+        """
+        Validates prediction score is in ranges or limits.
+        """
+        if v not in ["A", "B", "C"]:
+            raise ValueError('Unkown value, must be a value in ["A", "B", "C"]')
+        return v
 
 class Response(BaseModel):
     """
@@ -35,6 +45,15 @@ class Response(BaseModel):
     All responses to credit scoring should abide by this format.
     """
     prediction_score : int
+
+    @validator('prediction_score')
+    def between_0_and_100(cls, v):
+        """
+        Validates prediction score is in range.
+        """
+        if not 0 <= v <= 100:
+            raise ValueError('must be a value between 0 and 100')
+        return v
 
 
 @app.post("/predict", response_model=Response)
@@ -46,7 +65,7 @@ def predict():
     # TODO: call your preprocessing function (check example below)
     # TODO: call your model's predict function (loaded at top of file)
 
-    return {"prediction_score": "50"}
+    return {"prediction_score": 42}
 
 """
 # example of what your predict endpoint might look like.
@@ -61,8 +80,8 @@ def predict(req: Request):
     predictions = model.predict(clean_data)
 
     # format predictions to the expected response format
-    formatted_predictions = format_preds(predictions['y'])
+    formatted_prediction = format_pred(prediction['y'])
 
-    return formatted_predictions
+    return formatted_prediction
 """
 
